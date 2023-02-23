f671a579a709d4f6a4d180764c3f0acc43f93572
Nermey <elik19898@gmail.com>
2023-02-23 16:33:08 +0300

    lab1

---
+++ lab1.py
@@ -0,0 +1,13 @@
+a = int(input())
+b = int(input())
+c = int(input())
+D = b**2 - 4 * a * c
+if D < 0:
+    print("Корней нет")
+else:
+    x1 = (-b + D**0.5) / (2 * a)
+    x2 = (-b - D**0.5) / (2 * a)
+    if x1 == x2:
+        print("Корень уравнения:", x1)
+    else:
+        print("Корни уравнения:", x1, x2)
